bce7c068ef4ebd14a70768cad8c9057e4a4df3be
Pedro Lopes <pedro.lopes@microsoft.com>
2017-11-03 13:54:10 -0700

    Replace with zip file

--- Gems-to-help-you-troubleshoot-query-performance/Demo-5-Live-Query-Troubleshooting/DISK3/Cleanup.sql
+++
@@ -1,5 +0,0 @@
-USE AdventureWorks2016CTP3
-GO
---IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[Sales].[SalesOrderDetail2]') AND type in (N'U'))
---DROP TABLE [Sales].[SalesOrderDetail2]
---GO
